60e8c10b8498a8aa41e73c62bee779921efa4609
César Palma Morante <cemp2703@gmail.com>
2022-06-16 11:54:06 -0500

    aside implementado

--- src/components/aside/index.tsx
+++ src/components/aside/index.tsx
@@ -1,9 +1,77 @@
-import './style.css';
+import classes from "./styles.module.css";
+import { pages, tools, printExport, projects, languages } from "./links.js";
+import esWiki from "@/assets/eswiki.png";
 
 export function Aside() {
   return (
     <aside>
-      <span>aside</span>
+      <section className={classes.logo}>
+        <a href="#">
+          <img src={esWiki} alt="wikipedia logo" />
+        </a>
+      </section>
+      <section>
+        <ul>
+          {pages.map(({ title, url }) => (
+            <li key={title}>
+              <a href={url}>{title}</a>
+            </li>
+          ))}
+        </ul>
+      </section>
+
+      <section>
+        <h2>
+          <span>Herramientas</span>
+        </h2>
+        <ul>
+          {tools.map(({ title, url }) => (
+            <li key={title}>
+              <a href={url}>{title}</a>
+            </li>
+          ))}
+        </ul>
+      </section>
+      <section>
+        <h2>
+          <span>Imprimir/exportar</span>
+        </h2>
+        <ul>
+          {printExport.map(({ title, url }) => (
+            <li key={title}>
+              <a href={url}>{title}</a>
+            </li>
+          ))}
+        </ul>
+      </section>
+      <section>
+        <h2>
+          <span>En otros proyectos</span>
+        </h2>
+        <ul>
+          {projects.map(({ title, url }) => (
+            <li key={title}>
+              <a href={url}>{title}</a>
+            </li>
+          ))}
+        </ul>
+      </section>
+      <section>
+        <h2>
+          <span>En otros idiomas</span>{" "}
+          <button title="Opciones de idioma">O</button>
+        </h2>
+        <ul>
+          {languages.map(({ title, url }) => (
+            <li key={title}>
+              <a href={url}>{title}</a>
+            </li>
+          ))}
+          <li>
+            <a href="#">Lista completa</a>
+          </li>
+        </ul>
+      </section>
     </aside>
   );
 }
---
+++ src/components/aside/links.ts
@@ -0,0 +1,59 @@
+type Page = {
+  url : string,
+  title : string,
+}
+
+export const pages: Page[] = [
+    {
+      title: "Portada",
+      url: "#",
+    },
+    {
+      title: "Portal de la comunidad",
+      url: "#",
+    },
+  ];
+
+  export const tools: Page[] = [
+    {
+      title: "Lo que enlaza aquí",
+      url: "#",
+    },
+    {
+      title: "Subir archivo",
+      url: "#",
+    }
+  ]
+
+  export const printExport: Page[] = [
+    {
+      title: "Crear un libro",
+      url: "#",
+    },
+    {
+      title: "Descargar como PDF",
+      url: "#",
+    }
+  ]
+
+  export const projects : Page[] = [
+    {
+      title: "Wikimedia Commons",
+      url: "#",
+    },
+    {
+      title: "MediaWiki",
+      url: "#",
+    }
+  ]
+
+  export const languages : Page[] = [
+    {
+      title: "Español",
+      url: "#",
+    },
+    {
+      title: "English",
+      url: "#",
+    }
+  ]
