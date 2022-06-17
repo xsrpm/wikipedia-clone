2fc9ae1baa9d177aee05204be691c0a3a83760dd
César Palma Morante <cemp2703@gmail.com>
2022-06-17 12:30:02 -0500

    agregado header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,9 +1,65 @@
-import './styles.css';
+import classes from "./styles.module.css";
+import userAvatar from "@/assets/images/user-avatar.svg";
+import tabNormalFade from "@/assets/images/tab-normal-fade.png";
 
 export function Header() {
   return (
     <header>
-      <span>header</span>
+      <div className={classes.personal}>
+        <div>
+          <img src={userAvatar} width="15px" height="15px" />
+          <span>No has accedido</span>
+        </div>
+        <ul>
+          <li>
+            <a href="">Discusión</a>
+          </li>
+          <li>
+            <a href="">Contribuciones</a>
+          </li>
+          <li>
+            <a href="">Crear una cuenta</a>
+          </li>
+          <li>
+            <a href="">Acceder</a>
+          </li>
+        </ul>
+      </div>
+      <ul className={classes.left}>
+        <li className={classes.selected}>
+          <a href="#">Portada</a>
+        </li>
+        <li>
+          <a href="#">Discusión</a>
+        </li>
+      </ul>
+      <div className={classes.right}>
+        <ul>
+          <li className={classes.selected}>
+            <a href="#">Leer</a>
+          </li>
+          <li>
+            <a href="#">Ver código fuente</a>
+          </li>
+          <li>
+            <a href="#">Ver historial</a>
+          </li>
+        </ul>
+        <div>
+          <div className={classes.searchInput}>
+            <input
+              type="search"
+              placeholder="Buscar en Wikipedia"
+              autoComplete="off"
+            />
+            <input
+              type="submit"
+              title="Ir a la página con este nombre exacto si existe"
+              value="Ir"
+            ></input>
+          </div>
+        </div>
+      </div>
     </header>
   );
 }
