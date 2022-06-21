58726e72b6d944228c5b6b972ca132a26888e825
César Palma Morante <cemp2703@gmail.com>
2022-06-21 18:39:07 -0500

    agregando titles a ancors - welcome

--- src/components/portada/welcome/index.tsx
+++ src/components/portada/welcome/index.tsx
@@ -6,34 +6,66 @@ export function Welcome() {
     <section className={classes.welcome}>
       <p>
         <span>
-          <a href="#">Bienvenidos</a> a Wikipedia,
+          <a href="#" title="Wikipedia:Bienvenidos">
+            Bienvenidos
+          </a>{" "}
+          a Wikipedia,
         </span>
         <br />
         la enciclopedia de contenido libre
         <br />
-        que <a href="#">todos pueden editar</a>.
+        que{" "}
+        <a href="#" title="Ayuda:Introducción">
+          todos pueden editar
+        </a>
+        .
       </p>
       <p>
         <b>
-          <a href="#">1 783 266</a>
+          <a href="#" title="Especial:Estadísticas">
+            1 783 266
+          </a>
         </b>{" "}
-        artículos en español
+        artículos{" "}
+        <a href="#" title="Wikipedia en español">
+          en español
+        </a>
       </p>
       <ul>
         <li>
-          <SoftLinkButton name="Cafe" padding="5px 0 5px 12px" />
+          <SoftLinkButton
+            name="Cafe"
+            padding="5px 0 5px 12px"
+            title="Wikipedia:Café"
+          />
         </li>
         <li>
-          <SoftLinkButton name="¿Como colaborar?" padding="5px 0 5px 12px" />
+          <SoftLinkButton
+            name="¿Como colaborar?"
+            padding="5px 0 5px 12px"
+            title="Ayuda:Cómo puedes colaborar"
+          />
         </li>
         <li>
-          <SoftLinkButton name="Primeros pasos" padding="5px 0 5px 12px" />
+          <SoftLinkButton
+            name="Primeros pasos"
+            padding="5px 0 5px 12px"
+            title="Ayuda:Introdución"
+          />
         </li>
         <li>
-          <SoftLinkButton name="Ayuda" padding="5px 0 5px 12px" />
+          <SoftLinkButton
+            name="Ayuda"
+            padding="5px 0 5px 12px"
+            title="Ayuda:Contenidos"
+          />
         </li>
         <li>
-          <SoftLinkButton name="Contacto" padding="5px 0 5px 12px" />
+          <SoftLinkButton
+            name="Contacto"
+            padding="5px 0 5px 12px"
+            title="Wikipedia:Contacto"
+          />
         </li>
       </ul>
     </section>
--- src/components/softLinkButton/index.tsx
+++ src/components/softLinkButton/index.tsx
@@ -3,11 +3,12 @@ import classes from "./styles.module.css";
 type props = {
   name: string;
   padding?: string;
+  title?: string;
 };
 
-export function SoftLinkButton({ name, padding }: props) {
+export function SoftLinkButton({ name, padding, title }: props) {
   return (
-    <a href="#" className={classes.softLinkButton}>
+    <a href="#" className={classes.softLinkButton} title={title}>
       <span style={{ padding }}>{name}</span>
     </a>
   );
