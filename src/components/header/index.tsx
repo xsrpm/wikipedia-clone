b4d084495764d6100336f2e969f85418402450a3
César Palma Morante <cemp2703@gmail.com>
2022-06-22 06:41:00 -0500

    titles para header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -8,29 +8,55 @@ export function Header() {
       <div className={classes.personal}>
         <div>
           <img src={userAvatar} width="15px" height="15px" />
-          <span>No has accedido</span>
+          <span title="La página de usuario de la IP desde la que editas">
+            No has accedido
+          </span>
         </div>
         <ul>
           <li>
-            <a href="">Discusión</a>
+            <a
+              href="#"
+              title="Discusión sobre ediciones hechas desde esta dirección IP [alt-shift-n]"
+            >
+              Discusión
+            </a>
           </li>
           <li>
-            <a href="">Contribuciones</a>
+            <a
+              href="#"
+              title="Una lista de modificaciones hechas desde esta dirección IP [alt-shift-y]"
+            >
+              Contribuciones
+            </a>
           </li>
           <li>
-            <a href="">Crear una cuenta</a>
+            <a
+              href="#"
+              title="Te recomendamos crear una cuenta e iniciar sesión; sin embargo, no es obligatorio"
+            >
+              Crear una cuenta
+            </a>
           </li>
           <li>
-            <a href="">Acceder</a>
+            <a
+              href="#"
+              title="Te recomendamos iniciar sesión, aunque no es obligatorio [alt-shift-o]"
+            >
+              Acceder
+            </a>
           </li>
         </ul>
       </div>
       <ul className={classes.left}>
         <li className={classes.selected}>
-          <a href="#">Portada</a>
+          <a href="#" title="Ver la página del proyecto [alt-shift-a]">
+            Portada
+          </a>
         </li>
         <li>
-          <a href="#">Discusión</a>
+          <a href="#" title="Discusión acerca de la página [alt-shift-t]">
+            Discusión
+          </a>
         </li>
       </ul>
       <div className={classes.right}>
@@ -39,10 +65,20 @@ export function Header() {
             <a href="#">Leer</a>
           </li>
           <li>
-            <a href="#">Ver código fuente</a>
+            <a
+              href="#"
+              title="Esta página está protegida, pero puedes ver su código fuente [alt-shift-e]"
+            >
+              Ver código fuente
+            </a>
           </li>
           <li>
-            <a href="#">Ver historial</a>
+            <a
+              href="#"
+              title="Versiones anteriores de esta página [alt-shift-h]"
+            >
+              Ver historial
+            </a>
           </li>
         </ul>
         <div>
@@ -51,6 +87,7 @@ export function Header() {
               type="search"
               placeholder="Buscar en Wikipedia"
               autoComplete="off"
+              title="Buscar en este wiki [alt-shift-f]"
             />
             <input
               type="submit"
